dcb5c2172622592488ef433afaeb1be7e9c0515f
Zikhona <zikhonalaurettapoto35@gmail.com>
2016-03-10 13:15:16 +0200

    Ballon kata

---
+++ balloon.js
@@ -0,0 +1,17 @@
+
+//    This order placed was:
+
+//     4 sets of red, 3 sets of blue, and 3 sets of yellow.
+
+
+// The price for each set of balloons is:
+
+//     R4 for red, R5 for blue, and R5.50 for yellow.
+
+
+
+    // How many of each colour balloon did we order?
+    // How much do all the balloons cost?
+    // If it costs R2.00 to inflate each balloon with helium, how much extra would that cost?
+    // If there are 25 people coming to the party, are there enough balloons for everyone to get one balloon? Do you have any spare?
+
---
+++ balloon_tests.js
@@ -0,0 +1,4 @@
+// QUnit.test("findTheString",function(assert) {
+// var  colorBalloon= findTheString("4 sets of red, 3 sets of blue, and 3 sets of yellow.");
+// assert.deepEqual(colorBalloon, [""]);
+// });
